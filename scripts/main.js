9b9899014597945ca2c94bf07449b87961c81f79
LucasD <delobelle.lucas@club-internet.fr>
2015-12-06 12:30:09 +0100

    réduction largeur layout, bg sur contenu texte, ajustements responsives

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,11 @@
 
 $(document).ready(function() {
   $('#wrapper').fadeIn(1200);
-  
+  /*
+  setTimeout(function() {
+    $('#wrapper').fadeIn(1200);
+  }, 2000);
+  */
   $(window).scroll(function() {
     if($(window).scrollTop() > 175) {
       $('#player').addClass('floating');
