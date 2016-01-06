4bf5c6f6cfd32663e35904c00ac05693319d9fdc
LucasD <delobelle.lucas@club-internet.fr>
2016-01-07 00:00:39 +0100

    intégration flags + navigation Ajax

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,22 @@
 /* Main */
 
+function load(lang) {
+  $.get(lang+".departure", function(data) {
+    $('#storyline_content').html(data);
+  });
+}
+
+function loadInit() {
+  var lang = (window.location.href.indexOf(".com") > -1 || window.location.href.indexOf("/en") > -1) ? "en" : "fr";  
+  load(lang);
+  return lang;
+}
+
 $(document).ready(function() {
+  var langInit = loadInit();
+  
+  history.pushState({key : langInit}, 'version : '+langInit, '/'+langInit);
+  
   $('#wrapper').delay(7500).fadeIn(1600, function() {
     $('body').css('overflow', 'auto');
     $('iframe').attr('src', 'http://bandcamp.com/EmbeddedPlayer/track=4293759057/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/transparent=true/');
@@ -15,4 +31,19 @@ $(document).ready(function() {
       $('#storyline').removeClass('marginStoryline');
     }
   });
+  
+  $('.flag').on('click', function(event) {
+    var lang = $(this).attr('data-lang');
+    event.preventDefault();
+    load(lang);
+    history.pushState({key : lang}, 'version : '+lang, '/'+lang);
+  });
+  
+  window.onpopstate = function(event) {
+    if(event.state == null) {
+      loadInit();
+    } else {
+      load(event.state.key);
+    }
+  }
 });
